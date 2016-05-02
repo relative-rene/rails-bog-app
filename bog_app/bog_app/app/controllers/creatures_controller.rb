4298155b16e480aa43d2b5b33240953276b84fc7
relative-rene <rene.arellano415@gmail.com>
2016-05-02 00:18:26 -0700

    complete restful

--- bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -2,14 +2,12 @@ class CreaturesController < ApplicationController
 
   def index
     @creatures = Creatures.all
-
     render :index
   end
 
   def new
     @creature = Creature.new
     render :new
-
   end
 
   def create
@@ -30,7 +28,6 @@ class CreaturesController < ApplicationController
   def edit
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
-
     render :edit
   end
 
@@ -39,7 +36,6 @@ class CreaturesController < ApplicationController
     creature = Creature.find_by_id(creature_id)
     creature_params = params.require(:creature).permit(:name, :description)
     creature.update_attributes(creature_params)
-
     redirect_to creature_path(creature)
   end
 
